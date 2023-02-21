c344e40a49e2e1d1e73ddefe8c8c873fcec64701
HeoYou <lov21y_@naver.com>
2023-02-22 01:21:46 +0900

    First commit

---
+++ src/main/java/com/wsb/sellinone/SellinoneApplication.java
@@ -0,0 +1,13 @@
+package com.wsb.sellinone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellinoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SellinoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wsb/sellinone/SellinoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wsb.sellinone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellinoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
